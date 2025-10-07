8c74946a99494e83a188d0dfd15d377941ac1347
ogg1996 <9migoAhri@gmail.com>
2025-10-08 01:49:11 +0900

    점검으로 인한 사이트 접근불가 시간 수정

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,8 @@ function App() {
   const today = now.getDay();
   const nowHours = now.getHours();
 
-  // 목요일 새벽 5시부터 오전 10시 전까지 사이트 접근을 막는 코드
-  // (점검 시간에는 API가 먹통이됨)
-  if (today === 4 && 5 <= nowHours && nowHours < 10)
+  // 목요일 0시부터 10시 전까지 사이트 접근을 막는 코드 (OPEN API 점검)
+  if (today === 4 && 0 <= nowHours && nowHours < 10)
     return <AccessDenied />;
 
   return (
