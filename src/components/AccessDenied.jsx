cbe35eec4418579d55ca4cce5e2308fcdc7b7b59
ogg1996 <9migoAhri@gmail.com>
2025-10-08 01:50:24 +0900

    접근제한 페이지 이용제한시간 텍스트 수정

--- src/components/AccessDenied.jsx
+++ src/components/AccessDenied.jsx
@@ -5,7 +5,7 @@ const AccessDenied = () => {
       font-bold"
     >
       <p className="text-[18px] pb-[16px]">
-        이용 제한 시간 &#40; 05 : 00 ~ 10 : 00 &#41;
+        이용 제한 시간 &#40; 00 : 00 ~ 10 : 00 &#41;
       </p>
       <p className="text-[22px]">던파 점검중에는</p>
       <p className="text-[22px]">서비스가 중단됩니다.</p>
